51fcec4e9e2655c25745a07d61ec6bcdd0c57cc3
ayush24k <ayushanshu69@gmail.com>
2025-06-19 11:14:39 +0530

    http express server setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log("hello")
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Hello from the server")
+})
+
+async function serverInit() {
+    const PORT = process.env.PORT ? process.env.PORT : 8000;
+
+    try {
+        const server = app.listen(PORT, () => {
+            console.log(`Server startd at port: ${PORT} \nlink: http://localhost:${PORT}`);
+        })
+    } catch (err) {
+        console.log("Error starting the server:", err);
+    }
+}
+
+serverInit();
